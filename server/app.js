6fea9228829996c29b218919566be4b59c0cd1b8
ShashankC10 <shashank3087@gmail.com>
2021-08-07 11:46:46 +0530

    made changes to server

--- server/app.js
+++ server/app.js
@@ -70,46 +70,7 @@ app.route("/articles/delete")
       }
     });}
     
-)
-.get(function(req, res){
-
-  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No articles matching that title was found.");
-    }
-  });
-})
-
-.put(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {title: req.body.title, content: req.body.content},
-    {overwrite: true},
-    function(err){
-      if(!err){
-        res.send("Successfully updated the selected article.");
-      }
-    }
-  );
-})
-
-.patch(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {$set: req.body},
-    function(err){
-      if(!err){
-        res.send("Successfully updated article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-})
+);
 
 
 
